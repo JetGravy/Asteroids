0d0464f6820516d1303dc2481a85f2672ac7607a
JetGravy <jetgravy@gmail.com>
2025-09-29 05:33:32 -0400

    set clock and dt

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from constants import *
 def main():
     pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # delta time
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print("Exiting Asteroids, did you have fun?")
                 return
-        screen.fill((0,0,0))
-        
+        screen.fill("black")
         pygame.display.flip()
+        
+        # resource management
+        dt = clock.tick(60) / 1000
+    
 
 
 if __name__ == "__main__":
