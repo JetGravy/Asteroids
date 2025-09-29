19cbee9c0d596504cf6a1260bcdad108adeafb5e
JetGravy <jetgravy@gmail.com>
2025-09-29 05:46:02 -0400

    player drawn on screen

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0 # delta time
 
     while True:
@@ -14,6 +16,7 @@ def main():
                 print("Exiting Asteroids, did you have fun?")
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         # resource management
